83a1599dae4b94508454bacc516914da9c88269c
stabmeldys <lukas.les93@gmail.com>
2024-10-16 17:52:26 +0300

    toggle neotree keymap adde

--- lua/keyconfig.lua
+++ lua/keyconfig.lua
@@ -1,3 +1,5 @@
+vim.keymap.set('n', '<leader>nt', '<Cmd>Neotree toggle<CR>', { desc = 'Toggle Neotree' })
+
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
