419d329839d5e48044aa364205ee1bc43626a9b5
stabmeldys <lukas.les93@gmail.com>
2024-10-18 15:22:29 +0300

    added telescope plugin

--- lua/plugins/ide.lua
+++ lua/plugins/ide.lua
@@ -25,6 +25,11 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    },
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    -- or                          , branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
     }
-}
 }
