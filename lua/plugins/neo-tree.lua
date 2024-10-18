d42b58e5a6ffb865621223d25467129008b291ba
stabmeldys <lukas.les93@gmail.com>
2024-10-18 20:23:43 +0300

    fix

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,8 +8,6 @@ return {
             "MunifTanjim/nui.nvim",
             -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         },
-    },
-    {
         config = function()
             require("neo-tree").setup({
                 close_if_last_window = true,
