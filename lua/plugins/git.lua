be613091a1aee08bc3613c35b9eba5b9e9362d15
stabmeldys <lukas.les93@gmail.com>
2024-10-19 11:23:09 +0300

    figutive cfg

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,8 +1,7 @@
 return  {
-    'tpope/vim-fugitive',
-    lazy = false,
+    "tpope/vim-fugitive",
     config = function()
-      vim.keymap.set('n', 'gs', '<Cmd>Git<CR>')
+      vim.keymap.set("n", "gs", "<Cmd>Git<CR>")
     end,
 }
 
