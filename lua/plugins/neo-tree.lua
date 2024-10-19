0f0e479e18ec899984b9b98c342ddbae3fccafa3
stabmeldys <lukas.les93@gmail.com>
2024-10-19 15:15:53 +0300

    fix neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,71 +1,68 @@
 return {
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            -- "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        },
-        config = function()
-            require("neo-tree").setup({
-                close_if_last_window = true,
-
-                default_component_configs = {
-                    indent = {
-                        expander_collapsed = "▶",
-                        expander_expanded = "▼",
-                    },
-                    icon = {
-                        folder_closed = "▶",
-                        folder_open = "▼",
-                        folder_empty = "▷",
-                        default = " ",
-                        highlight = "NeoTreeFileIcon",
-                    },
-                    name = {
-                        use_git_status_colors = false,
-                    },
-                    git_status = {
-                        symbols = {
-                            added = "",
-                            modified = "✹",
-                            deleted = "✖",
-                            renamed = "➜",
-                            untracked = "✭",
-                            ignored = "☒",
-                            unstaged = "✗",
-                            staged = "+",
-                            conflict = "C",
-                        },
-                    },
-                },
-
-                filesystem = {
-                    filtered_items = {
-                        hide_dotfiles = false,
-                        hide_gitignored = false,
-                        never_show = { ".git" },
-                    },
-                },
-                window = {
-                    position = "left",
-                    mappings = {
-                        ["oo"] = { "open", nowait = true },
-                        ["Y"] = "copy_filename_to_clipboard",
-                        ["<C-r>"] = "set_root",
-                    },
-                },
-                commands = {
-                    copy_filename_to_clipboard = function(state)
-                        local node = state.tree:get_node()
-                        local filename = node.name
-                        vim.fn.setreg('+', filename)  -- Copy to system clipboard
-                        print('Copied: ' .. filename .. ' to clipboard')
-                    end
-                },
-            )
-        end,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        -- "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window
     },
+    config = function()
+        require("neo-tree").setup({
+            close_if_last_window = true,
+            default_component_configs = {
+                indent = {
+                    expander_collapsed = "▶",
+                    expander_expanded = "▼",
+                },
+                icon = {
+                    folder_closed = "▶",
+                    folder_open = "▼",
+                    folder_empty = "▷",
+                    default = " ",
+                    highlight = "NeoTreeFileIcon",
+                },
+                name = {
+                    use_git_status_colors = false,
+                },
+                git_status = {
+                    symbols = {
+                        added = "",
+                        modified = "✹",
+                        deleted = "✖",
+                        renamed = "➜",
+                        untracked = "✭",
+                        ignored = "☒",
+                        unstaged = "✗",
+                        staged = "+",
+                        conflict = "C",
+                    },
+                },
+            },
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                    never_show = { ".git" },
+                },
+            },
+            window = {
+                position = "left",
+                mappings = {
+                    ["oo"] = { "open", nowait = true },
+                    ["Y"] = "copy_filename_to_clipboard",
+                    ["<C-r>"] = "set_root", -- Setting root with Ctrl + r
+                },
+            },
+            commands = {
+                copy_filename_to_clipboard = function(state)
+                    local node = state.tree:get_node()
+                    local filename = node.name
+                    vim.fn.setreg('+', filename) -- Copy to system clipboard
+                    print('Copied: ' .. filename .. ' to clipboard')
+                end
+            },
+        })
+    end,
 }
+
