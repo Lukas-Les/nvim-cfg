8a083e7540188633e64794dfcf1fcc58d71e8286
stabmeldys <lukas.les93@gmail.com>
2024-10-19 13:52:25 +0300

    added terminal binds

--- lua/keyconfig.lua
+++ lua/keyconfig.lua
@@ -1,5 +1,9 @@
 vim.keymap.set("n", "<leader>nt", "<Cmd>Neotree toggle<CR>", { desc = "Toggle Neotree" })
 
+vim.keymap.set('n', '<leader>tt', ':vsplit | term<CR>', { noremap = true, silent = true })  -- Vertical split terminal
+vim.keymap.set('n', '<leader>th', ':split | term<CR>', { noremap = true, silent = true })   -- Horizontal split terminal
+
+
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
