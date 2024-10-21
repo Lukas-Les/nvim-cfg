3b9e2fc7e2e5452a8eb6b1c38a1349b1f853b35c
stabmeldys <lukas.les93@gmail.com>
2024-10-21 17:17:45 +0300

    fix

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -1,8 +1,52 @@
 return {
-    'rcarriga/nvim-dap-ui',
-    dependencies = {'mfussenegger/nvim-dap'},
+  -- Load nvim-dap
+  {
+    'mfussenegger/nvim-dap',
     config = function()
-      require('plugins.dap')  -- This will load the init file in `lua/plugins/dap/`
-    end
+      local dap = require('dap')
+
+      -- Python DAP Configuration
+      dap.adapters.python = {
+        type = 'executable',
+        command = 'python', -- Adjust if using a virtual environment
+        args = { '-m', 'debugpy.adapter' },
+      }
+
+      dap.configurations.python = {
+        {
+          type = 'python',
+          request = 'launch',
+          name = "Launch file",
+          program = "${file}",
+          pythonPath = function()
+            return 'python'
+          end,
+        },
+      }
+
+      -- Add configurations for other languages (e.g., Rust) here
+    end,
+  },
+
+  -- Load nvim-dap-ui and link it to nvim-dap
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap' },
+    config = function()
+      local dapui = require('dapui')
+      dapui.setup()
+
+      local dap = require('dap')
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
 }
 
--- lua/plugins/dap/init.lua
+++
@@ -1,2 +0,0 @@
-require("plugins.dap.python")  -- Load Python configuration
-
