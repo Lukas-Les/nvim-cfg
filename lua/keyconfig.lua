44b7016940a2e3bb0f178d7a245a62e0f1080b96
stabmeldys <lukas.les93@gmail.com>
2024-10-21 15:59:42 +0300

    vimspector key mappings moved to plugin file

--- lua/keyconfig.lua
+++ lua/keyconfig.lua
@@ -9,16 +9,4 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
--- Vimspector
-vim.cmd([[
-nmap <F9> <cmd>call vimspector#Launch()<cr>
-nmap <F5> <cmd>call vimspector#StepOver()<cr>
-nmap <F8> <cmd>call vimspector#Reset()<cr>
-nmap <F11> <cmd>call vimspector#StepOver()<cr>")
-nmap <F12> <cmd>call vimspector#StepOut()<cr>")
-nmap <F10> <cmd>call vimspector#StepInto()<cr>")
-]])
-vim.keymap.set('n', "Db", ":call vimspector#ToggleBreakpoint()<cr>")
-vim.keymap.set('n', "Dw", ":call vimspector#AddWatch()<cr>")
-vim.keymap.set('n', "De", ":call vimspector#Evaluate()<cr>")
 
