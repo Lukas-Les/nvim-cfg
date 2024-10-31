af4b2e9a337c2b1c56bba02cb0e0f7abeef75bb4
stabmeldys <lukas.les93@gmail.com>
2024-10-31 10:30:10 +0200

    pylsp and cmp plugins added

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,10 +1,60 @@
 return {
-  'neovim/nvim-lspconfig',
-  config = function()
-    local lspconfig = require('lspconfig')
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      local lspconfig = require('lspconfig')
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-    -- Setup Ruff LSP
-    lspconfig.ruff_lsp.setup({})
-  end,
+      -- Setup Ruff LSP for linting
+      lspconfig.ruff_lsp.setup({
+        capabilities = capabilities,
+        filetypes = { "python" },
+      })
+
+      -- Replace 'pylsp' with 'jedi_language_server' if using that instead
+      lspconfig.pylsp.setup({
+        capabilities = capabilities,
+        filetypes = { "python" },
+      })
+    end,
+  },
+
+  -- nvim-cmp and its sources
+  { 'hrsh7th/cmp-nvim-lsp' },
+  {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/vim-vsnip',
+    },
+    config = function()
+      local cmp = require('cmp')
+
+      vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        mapping = {
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ['<Tab>'] = cmp.mapping.select_next_item(),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.close(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        },
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'buffer' },
+          { name = 'path' },
+        })
+      })
+    end,
+  }
 }
 
