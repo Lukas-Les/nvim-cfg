31f51d84d2d7ef5bf7fe3710316ca0205c1e70b6
stabmeldys <lukas.les93@gmail.com>
2024-11-23 11:23:59 +0200

    lazy.nvim re-applied

--- init.lua
+++ init.lua
@@ -6,7 +6,6 @@ vim.g.maplocalleader = " "
 vim.g.t_co = 256 -- enabling support for 256 colors in whichever terminal emulator
 vim.g.background = "dark" -- terminal background will use dark or light mode
 
-
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = false
 
@@ -52,47 +51,52 @@ vim.opt.tabstop = 4 -- An indentation every four columns
 vim.opt.softtabstop = 4 -- Let backspace delete indent
 vim.opt.smartindent = true -- Automatically inserts one extra level of indentation in some cases
 
-
-
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
+-- Setup lazy.nvim
+require("lazy").setup("plugins", {
     spec = {
-        { import = "plugins" }, -- This imports all plugin files from lua/plugins/
+        -- add your plugins here
     },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "habamax" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
     ui = {
-        -- If you have a Nerd Font, set icons to an empty table which will use the
-        -- default lazy.nvim defined Nerd Font icons otherwise define a unicode icons table
         icons = vim.g.have_nerd_font and {} or {
             cmd = "⌘",
             config = "🛠",
             event = "📅",
+            favorite = "⭐",
             ft = "📂",
             init = "⚙",
+            import = "📥",
             keys = "🗝",
+            lazy = "💤 ",
             plugin = "🔌",
             runtime = "💻",
             require = "🌙",
             source = "📄",
             start = "🚀",
             task = "📌",
-            lazy = "💤 ",
         },
     },
 })
--- [[ Keyconfig ]]
-require("keyconfig")
 
+require("keyconfig")
