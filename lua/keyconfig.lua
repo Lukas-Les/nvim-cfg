cffb87981a470a8f9e291480b4b5f5342357bfb6
Lukas Leščinskas <71329803+Lukas-Les@users.noreply.github.com>
2025-04-14 13:32:28 +0300

    do not replace clipboard then pasting

--- lua/keyconfig.lua
+++ lua/keyconfig.lua
@@ -21,3 +21,11 @@ vim.keymap.set(
     "<Cmd>set invhlsearch<CR>",
     { remap = true, silent = true, desc = "Hide search highlights" }
 )
+
+-- If you visually select something and hit paste
+-- that thing gets yanked into your buffer. This
+-- generally is annoying when you're copying one item
+-- and repeatedly pasting it. This changes the paste
+-- command in visual mode so that it doesn't overwrite
+-- whatever is in your paste buffer.
+vim.keymap.set('v', 'p', '"_dP"')
