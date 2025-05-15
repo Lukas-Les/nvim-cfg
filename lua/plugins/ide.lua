30f00972f3ac653fc92a2c149afe41fd5abea637
Lukas Leščinskas <71329803+Lukas-Les@users.noreply.github.com>
2025-05-15 07:55:15 +0300

    don't autouse isort on save

--- lua/plugins/ide.lua
+++ lua/plugins/ide.lua
@@ -303,7 +303,7 @@ return {
                 -- Disable "format_on_save lsp_fallback" for languages that don't
                 -- have a well standardized coding style. You can add additional
                 -- languages here or re-enable it for the disabled ones.
-                local disable_filetypes = { c = true, cpp = true }
+                local disable_filetypes = { c = true, cpp = true, python = true }
                 if disable_filetypes[vim.bo[bufnr].filetype] then
                     return nil
                 else
@@ -315,7 +315,7 @@ return {
             end,
             formatters_by_ft = {
                 lua = { "stylua" },
-                python = { "isort", "black" },
+                python = { "black" },
                 xml = { "tidy" },
                 json = { "jquery" },
                 -- You can use 'stop_after_first' to run the first available formatter from the list
