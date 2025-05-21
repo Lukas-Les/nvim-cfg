b19dbe5c5325855104eef1f8cec0af428dffdff5
Lukas Leščinskas <71329803+Lukas-Les@users.noreply.github.com>
2025-05-21 06:51:16 +0300

    lualine

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -509,4 +509,32 @@ return {
             vim.keymap.set("n", "<leader>ntr", "<Cmd>Neotree reveal<CR>")
         end,
     },
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = {
+            options = {
+                icons_enabled = false,
+                theme = "dracula",
+                component_separators = { left = "", right = "" },
+                section_separators = { left = "", right = "" },
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch", "diff", "diagnostics" },
+                lualine_c = { "filename" },
+                lualine_x = { "encoding", "fileformat", "filetype" },
+                lualine_y = { "progress" },
+                lualine_z = { "location" },
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
+            extensions = { "neo-tree", "nvim-dap-ui" },
+        },
+    },
 }
