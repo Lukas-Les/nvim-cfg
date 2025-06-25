a86ca47a5b7bc26e27339e4be532b95b31145cca
stabmeldys <lukas.les93@gmail.com>
2025-06-25 17:02:24 +0300

    codecompanion additional key maps

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -29,6 +29,24 @@ return {
             },
         })
         vim.keymap.set("n", "<leader>cc", "<cmd>CodeCompanionChat Toggle<CR>", { desc = "[C]odeCompanion [C]hat" })
-        vim.keymap.set("n", "<leader>ca", "<cmd>CodeCompanionActions<CR>", { desc = "[C]odeCompanion [C]hat" })
+        vim.keymap.set("n", "<leader>ca", "<cmd>CodeCompanionActions<CR>", { desc = "[C]odeCompanion [A]ctions" })
+        vim.keymap.set(
+            { "n", "v" },
+            "<leader>ce",
+            "<cmd>CodeCompanion /explain<CR>",
+            { desc = "[C]odeCompanion [E]xplain" }
+        )
+        vim.keymap.set(
+            { "n", "v" },
+            "<leader>cr",
+            "<cmd>CodeCompanion /review<CR>",
+            { desc = "[C]odeCompanion [R]eview" }
+        )
+        vim.keymap.set(
+            { "n", "v" },
+            "<leader>ci",
+            "<cmd>CodeCompanion<CR>",
+            { desc = "[C]odeCompanion [I]nline Suggestion" }
+        )
     end,
 }
