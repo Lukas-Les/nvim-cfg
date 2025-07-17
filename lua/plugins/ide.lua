7829c986e6358a2de0dcb62da1e7b2f40d04565f
stabmeldys <lukas.les93@gmail.com>
2025-07-17 18:11:16 +0300

    html formatter

--- lua/plugins/ide.lua
+++ lua/plugins/ide.lua
@@ -318,6 +318,7 @@ return {
                 python = { "black" },
                 xml = { "tidy" },
                 json = { "jq" },
+                html = { "prettier" },
                 -- You can use 'stop_after_first' to run the first available formatter from the list
                 -- javascript = { "prettierd", "prettier", stop_after_first = true },
             },
