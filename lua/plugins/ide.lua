67b605ebe81b4d2b5a0546098ba9c1fe386e2157
lukas <lukas.les93@gmail.com>
2026-06-21 16:27:08 +0300

    fix tab related issue on Go files

--- lua/plugins/ide.lua
+++ lua/plugins/ide.lua
@@ -365,10 +365,7 @@ return {
                     --   end,
                     -- },
                 },
-                opts = {
-                    region_check_events = "InsertEnter",
-                    delete_check_events = "TextChanged",
-                },
+                opts = {},
             },
             "folke/lazydev.nvim",
         },
@@ -377,31 +374,27 @@ return {
         opts = {
             keymap = {
                 ["<CR>"] = { "accept", "fallback" },
-                -- 'default' (recommended) for mappings similar to built-in completions
-                --   <c-y> to accept ([y]es) the completion.
-                --    This will auto-import if your LSP supports it.
-                --    This will expand snippets if the LSP sent a snippet.
-                -- 'super-tab' for tab to accept
-                -- 'enter' for enter to accept
-                -- 'none' for no mappings
-                --
-                -- For an understanding of why the 'default' preset is recommended,
-                -- you will need to read `:help ins-completion`
-                --
-                -- No, but seriously. Please read `:help ins-completion`, it is really good!
-                --
-                -- All presets have the following mappings:
-                -- <tab>/<s-tab>: move to right/left of your snippet expansion
-                -- <c-space>: Open menu or open docs if already open
-                -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-                -- <c-e>: Hide menu
-                -- <c-k>: Toggle signature help
-                --
-                -- See :h blink-cmp-config-keymap for defining your own keymap
                 preset = "default",
-
-                -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-                --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+                ["<Tab>"] = {
+                    function()
+                        local luasnip = require("luasnip")
+                        if luasnip.locally_jumpable(1) then
+                            luasnip.jump(1)
+                            return true
+                        end
+                    end,
+                    "fallback",
+                },
+                ["<S-Tab>"] = {
+                    function()
+                        local luasnip = require("luasnip")
+                        if luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                            return true
+                        end
+                    end,
+                    "fallback",
+                },
             },
 
             appearance = {
